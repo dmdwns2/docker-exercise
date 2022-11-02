74c14a76c0646aea0122abff584b40b66fdfe438
dmdwns2 <ssreki1@naver.com>
2022-11-02 13:44:51 +0900

    [docker]-전국 병원 정보 연습 HospitalController 작성중

--- src/main/java/com/example/demo/controller/HospitalController.java
+++ src/main/java/com/example/demo/controller/HospitalController.java
@@ -2,9 +2,14 @@ package com.example.demo.controller;
 
 import com.example.demo.Dao.HospitalDao;
 import com.example.demo.domain.Hospital;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -18,10 +23,21 @@ public class HospitalController {
 
 //     1병원 이름, 2주소, 3도로명주소, 4의료진 수, 5병상 수, 6면적
 
-    @GetMapping("/name")
-    public Hospital name() {
-        return hospitalDao.findById(110000); // name부분만 어떻게 가져올지
-    }
+//    @GetMapping("/name3")
+//    public Hospital name3() {
+//        return hospitalDao.findById(110000); // name부분만 어떻게 가져올지
+//    }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Hospital> get(@PathVariable Integer id) {
+        Hospital hospital = hospitalDao.findById(id);
+        Optional<Hospital> opt = Optional.of(hospital);
+
+        if (!opt.isEmpty()) {
+            return ResponseEntity.ok().body(hospital);
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Hospital());
+        }
+    }
 }
 
